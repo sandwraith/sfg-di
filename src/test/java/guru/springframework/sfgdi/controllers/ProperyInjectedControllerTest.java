f80b9ca9fab38a5a6bd11d82d7d5b58121184891
Barış Selayet <bselayet@maritimetrainer.com>
2020-02-04 14:42:52 +0300

    Before Dependency Injection Assignment

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ProperyInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorInjectedGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProperyInjectedControllerTest {
+
+    ProperyInjectedController controller;
+
+    @BeforeEach
+    void setUp(){
+        controller=new ProperyInjectedController();
+
+        controller.greetingService = new ConstructorInjectedGreetingService();
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
